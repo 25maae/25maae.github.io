5218a44248667be4b3775d12fbb31e419646dd63
25maae <eaa25maae@students.eaaa.dk>
2026-08-12 19:29:17 +0200

    Integrate ReactLenis for smooth scrolling and add utility file for Lenis

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,24 @@ import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProjectPage from "./pages/ProjectPage";
 import ProjectsPage from "./pages/ProjectsPage";
+import { ReactLenis } from "/utils/lenis";
 
 function App() {
   return (
     <>
       <Navbar />
-
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/projects/:slug" element={<ProjectPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </main>
+      <ReactLenis root>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/projects/:slug" element={<ProjectPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+      </ReactLenis>
     </>
   );
 }
