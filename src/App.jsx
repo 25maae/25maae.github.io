1500a9ba13c864c1b347277e88970e5b2f2c42b4
25maae <eaa25maae@students.eaaa.dk>
2026-08-13 18:01:57 +0200

    Add scroll behavior and location tracking in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
@@ -6,9 +7,17 @@ import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProjectPage from "./pages/ProjectPage";
 import ProjectsPage from "./pages/ProjectsPage";
-import { ReactLenis } from "/utils/lenis";
+import { ReactLenis, useLenis } from "/utils/lenis";
 
 function App() {
+  const location = useLocation();
+  const lenis = useLenis();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    lenis?.scrollTo(0, { immediate: true });
+  }, [location.pathname, lenis]);
+
   return (
     <>
       <Navbar />
