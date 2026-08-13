c53196b7e86fbc024a31e624e23528c682266e0b
25maae <eaa25maae@students.eaaa.dk>
2026-08-13 23:46:05 +0200

    Add new SVG files for IPA and Triple Bock logos
    - Added ipa.svg (1.036 MB) to the public directory.
    - Added triple-bock.svg (1.503 MB) to the public directory.

--- src/data/projects.js
+++ src/data/projects.js
@@ -39,8 +39,8 @@ const projects = [
     year: "2026",
     summary: "Tøj webshop.",
     description:
-      "Wanted Archives er en webshop, der tilbyder en bred vifte af tøj og accessoires.",
-    tags: ["Design", "Adobe Photoshop", "Customer Experience", "Figma"],
+      "Wanted Archives er en webshop, der tilbyder en bred vifte af tøj og accessoires. Webshoppen er designet med fokus på brugeroplevelse og æstetik, og er bygget med HTML, CSS, JavaScript og hostet på GitHub Pages.",
+    tags: ["Design", "Adobe Photoshop", "Customer Experience", "Figma", "Web Shop", "JavaScript"],
     image: `${import.meta.env.BASE_URL}wanted-mockup.jpg`,
     detailImages: [
       `${import.meta.env.BASE_URL}wanted-mockup.jpg`,
@@ -60,56 +60,44 @@ const projects = [
     ],
   },
   {
-    slug: "Hoomie",
-    title: "Hoomie",
+    slug: "to-oel",
+    title: "To Øl - Etiketter/labels",
     year: "2026",
     summary:
-      "Webapp i mobil-first design, som samler opgaver, indkøb og planer under samme tag.",
+      "Etiketter design til To Øl.",
     description:
-      "Hoomie er en app, der hjælper roommates med at organisere deres hverdag og holde styr på opgaver, indkøb, planer m.m. Redigér, slet og tilføj opgaver, og få et overblik over, hvem der har gjort hvad. Appen er bygget med React og Vite, og er hostet på GitHub Pages.",
+      "To Øl er et dansk bryggeri, der producerer øl af høj kvalitet. Fokus har været at designe etiketter til deres øl, som afspejler deres brandidentitet og værdier. Designet er lavet i Adobe Illustrator. Dette er ikke et officielt samarbejde med To Øl!",
     tags: [
-      "React & Vite",
-      "Web Application",
-      "CRUD",
-      "Figma",
-      "React Router",
-      "Supabase",
+      "Adobe Illustrator",
+      "Brand Identity",
+      "Visual Design",
+      "Mockups",
     ],
-    image: `${import.meta.env.BASE_URL}hoomie-mockup.jpg`,
+    image: `${import.meta.env.BASE_URL}beer-mockup.svg`,
     detailImages: [
-      `${import.meta.env.BASE_URL}hoomie-mockup.jpg`,
-      `${import.meta.env.BASE_URL}hoomie-phones-mockup.jpg`,
-      `${import.meta.env.BASE_URL}hoomie-styletile.svg`,
-      `${import.meta.env.BASE_URL}hoomie-moodboard.svg`,
+      `${import.meta.env.BASE_URL}beers-mockup.jpg`,
+      `${import.meta.env.BASE_URL}berliner-weisse.svg`,
+      `${import.meta.env.BASE_URL}ipa.svg`,
+      `${import.meta.env.BASE_URL}triple-bock.svg`,
     ],
     links: [
-      {
-        label: "Udforsk projekt",
-        href: "https://sebbbenator.github.io/hoomie/",
-      },
-      {
-        label: "GitHub repository",
-        href: "https://github.com/Sebbbenator/hoomie",
-      },
     ],
   },
   {
-    slug: "Hoomie",
-    title: "Hoomie",
+    slug: "merkur",
+    title: "Merkur Andelskasse",
     year: "2026",
     summary:
-      "Webapp i mobil-first design, som samler opgaver, indkøb og planer under samme tag.",
+      "Redesign af Merkur Andelskasses logo og visuelle identitet.",
     description:
-      "Hoomie er en app, der hjælper roommates med at organisere deres hverdag og holde styr på opgaver, indkøb, planer m.m. Redigér, slet og tilføj opgaver, og få et overblik over, hvem der har gjort hvad. Appen er bygget med React og Vite, og er hostet på GitHub Pages.",
+      "Merkur Andelskasse er en dansk bank, der fokuserer på bæredygtighed og social ansvarlighed. Fokus har været at redesigne deres logo og visuelle identitet, så det afspejler deres værdier og vission. Designet er lavet i Adobe Illustrator. Dette er ikke et officielt samarbejde med Merkur Andelskasse!",
     tags: [
-      "React & Vite",
-      "Web Application",
-      "CRUD",
-      "Figma",
-      "React Router",
-      "Supabase",
+      "Adobe Illustrator",
+      "Brand Identity",
+      "Visual Design",
+      "Mockups",
     ],
-    image: `${import.meta.env.BASE_URL}hoomie-mockup.jpg`,
+    image: `${import.meta.env.BASE_URL}merkur-mockup.png`,
     detailImages: [
       `${import.meta.env.BASE_URL}hoomie-mockup.jpg`,
       `${import.meta.env.BASE_URL}hoomie-phones-mockup.jpg`,
@@ -117,14 +105,6 @@ const projects = [
       `${import.meta.env.BASE_URL}hoomie-moodboard.svg`,
     ],
     links: [
-      {
-        label: "Udforsk projekt",
-        href: "https://sebbbenator.github.io/hoomie/",
-      },
-      {
-        label: "GitHub repository",
-        href: "https://github.com/Sebbbenator/hoomie",
-      },
     ],
   },
 ];
